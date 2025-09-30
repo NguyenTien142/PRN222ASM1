ca421259c1f81803c60504a5eb598196c6b24756
ThangDVSE183732 <thangdvse183732@fpt.edu.vn>
2025-09-30 21:09:43 +0700

    Create: Admin_Layout

--- PRN222MVC/ElectricVehicleDealerManagermentSystem/Controllers/HomeController.cs
+++ PRN222MVC/ElectricVehicleDealerManagermentSystem/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ElectricVehicleDealerManagermentSystem.Controllers
             return View();
         }
 
+        public IActionResult Services()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
