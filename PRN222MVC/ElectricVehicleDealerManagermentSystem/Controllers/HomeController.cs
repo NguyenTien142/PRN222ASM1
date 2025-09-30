07e727a4c5ee01269a799989bacb18f39c271f87
ThangDVSE183732 <thangdvse183732@fpt.edu.vn>
2025-09-30 14:14:01 +0700

    Create:  Homepage

--- PRN222MVC/ElectricVehicleDealerManagermentSystem/Controllers/HomeController.cs
+++ PRN222MVC/ElectricVehicleDealerManagermentSystem/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ElectricVehicleDealerManagermentSystem.Controllers
             return View();
         }
 
+        public IActionResult PortfolioDetail()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
